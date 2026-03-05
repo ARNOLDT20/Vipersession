976294294af3129927007f71bba9e23ef602bffd
ARNOLD <atarimo117@gmail.com>
2026-03-05 12:50:48 -0800

    v

--- pair.js
+++ pair.js
@@ -11,7 +11,7 @@ const {
     delay,
     makeCacheableSignalKeyStore,
     Browsers
-} = require("@whiskeysockets/baileys");
+} = require("gifted-baileys");
 
 function removeFile(FilePath) {
     if (!fs.existsSync(FilePath)) return false;
